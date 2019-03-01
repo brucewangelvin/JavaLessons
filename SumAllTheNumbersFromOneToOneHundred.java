bea97a9065246543f7eb808984a83d9a975e3aee
MakeTheBrainHappy <33180528+MakeTheBrainHappy@users.noreply.github.com>
2019-03-01 16:06:50 -0500

    Made instructions more clear

--- SumAllTheNumbersFromOneToOneHundred.java
+++ SumAllTheNumbersFromOneToOneHundred.java
@@ -1,4 +1,4 @@
-/* Sum up all of the numbers from one to one hundred (1 + 2 + 3 + ... + 98 + 99 + 100). The answer is 5050. */
+/* Sum up all of the numbers from one to one hundred (1 + 2 + 3 + ... + 98 + 99 + 100) & print out the result. The answer is 5050. */
 
 public class SumAllTheNumbersFromOneToOneHundred {
 
