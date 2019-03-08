a5161a5fbd8ec1cd6a6009b858e5182be5d22586
Dark-Knight <yanling.elvin@gmail.com>
2019-03-08 15:45:49 -0500

    Finished SumAllTheNumbersFromOneToOneHundred

--- SumAllTheNumbersFromOneToOneHundred.java
+++ SumAllTheNumbersFromOneToOneHundred.java
@@ -1,5 +1,15 @@
 /* Sum up all of the numbers from one to one hundred (1 + 2 + 3 + ... + 98 + 99 + 100) & print out the result. The answer is 5050. */
 
 public class SumAllTheNumbersFromOneToOneHundred {
+    public static void main(String[] args) {
+      int n = 100;
+      int sum = 0;
 
+      for (int i = 0; i <= n; i++) {
+        sum = sum + i;
+      }
+     System.out.println(sum);
+
+
+    }
 }
