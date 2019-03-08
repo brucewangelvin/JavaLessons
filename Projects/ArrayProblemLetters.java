5128a070c684521fddb889c4c33377cb71677028
Dark-Knight <yanling.elvin@gmail.com>
2019-03-08 16:31:37 -0500

    Finished ArrayLetter problem
    Co-Authored-By: MakeTheBrainHappy <makethebrainhappy@users.noreply.github.com>

--- Projects/ArrayProblemLetters.java
+++ Projects/ArrayProblemLetters.java
@@ -16,9 +16,21 @@ public class ArrayProblemLetters {
         char[] letters = {'z','e','f','j','a','o','l','k','t'};
         char[] realLetters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
-        /* Code goes here */
+        char data[] = new char[letters.length];
+        int x = 0;
 
-        System.out.println(Arrays.toString(letters));
+        for (int j = 0; j < realLetters.length; j++){
+            for (int i = 0; i < letters.length; i++){
+                 if (letters[i]==realLetters[j]) {
+                     data[x]=letters[i];
+                     x+=1;
+                 }
+
+            }
+
+        }
+
+        System.out.println(Arrays.toString(data));
 
     }
 }
