493a22e93b306e3d67ac41e20215e2c0bd1c86ef
MakeTheBrainHappy <makethebrainhappy@gmail.com>
2019-03-09 14:37:25 -0500

    New Problem: Scanners & Arrays

---
+++ Projects/ScannerInput.java
@@ -0,0 +1,12 @@
+/*Create a program which allows a user to insert ten numbers into an array through the console.*/
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ScannerInput {
+    public static void main(String[] args) {
+
+        /*Code goes here*/
+
+    }
+}
