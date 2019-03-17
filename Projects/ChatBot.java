819b58edb913e75087c8ad338c0ce1bfa4ba08b0
MakeTheBrainHappy <makethebrainhappy@gmail.com>
2019-03-17 11:32:29 -0400

    Added a problem: ChatBot.java
    See the comment instructions.

---
+++ Projects/ChatBot.java
@@ -0,0 +1,38 @@
+/*
+
+Create a chat bot which can respond to a few basic text inputs.
+
+Here is an example conversation:
+
+Bot: Hi! What is your name?
+
+User: My name is _____.
+
+Bot: That's a great name! How are you today?
+
+User: Great!
+
+Bot: Nice to hear!
+
+Note: The bot should reply with words as appropriate to the context.
+If the user responds with "Not well" to the "How are you" question,
+then the bot should not respond with "nice to hear"
+
+Second note: the .contains() method is a great way to check whether
+a string has a certain substring (word) inside of it.
+
+You can choose more questions & customize its responses.
+
+The conversation should take place in the console.
+
+*/
+
+import java.util.Scanner;
+
+public class ChatBot {
+    public static void main(String[] args) {
+
+        /*Code goes here*/
+        
+    }
+}
