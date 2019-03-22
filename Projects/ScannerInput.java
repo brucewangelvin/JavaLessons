9884885cdc458f28db4b26ecfed354cf6a804054
brucewangelvin <48138018+brucewangelvin@users.noreply.github.com>
2019-03-22 16:04:24 -0400

    Update ScannerInput.java

--- Projects/ScannerInput.java
+++ Projects/ScannerInput.java
@@ -6,7 +6,14 @@ import java.util.Arrays;
 public class ScannerInput {
     public static void main(String[] args) {
 
-        /*Code goes here*/
+      int sum=0;
+     Scanner sc=new Scanner(System.in);
+     for(int i=1;i<=10;i++){
+         System.out.print(i+"-Enter Number:");
+         int c=sc.nextInt();
+     }
+    System.out.println("Sum of all the entered 10 numbers is:"+sum);
+     sc.close();
 
     }
 }
