71a7f8a4b17a389b0df9e44fb4eb62b4d69c417a
MakeTheBrainHappy <makethebrainhappy@gmail.com>
2019-05-08 12:53:55 -0400

    Added the circle class problem code

---
+++ Projects/CircleClass.java
@@ -0,0 +1,20 @@
+/*
+Create a class to represent a circle and include one method to find the area of the circle given the radius as instance data.
+
+Test this method using the "tester" class.
+*/
+
+public class CircleClass {
+
+    private int radius;
+
+    public CircleClass (int inputRadius) { // Complete the constructor
+
+    }
+
+    public int getArea() {
+        return ; 
+
+        // Complete the method. Formula for Area: pi (a constant (number)) * r^2 (or r*r)
+    }
+}
---
+++ Projects/CircleTester.java
@@ -0,0 +1,9 @@
+public class CircleTester {
+    public static void main(String[] args) {
+
+        CircleClass newCircle = new CircleClass(4); // This command will not work until you have created a constructor
+
+        System.out.println(newCircle.getArea()); // This command will not work until you have created a method
+
+    }
+}
